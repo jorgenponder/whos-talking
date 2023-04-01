385568f4811cd4e907eedfe72b0f15684775a253
user <user [at] example.com>
2023-04-01 19:36:06 +0200

    Correct indentation error

--- split.py
+++ split.py
@@ -4,33 +4,10 @@ import subprocess
 from pathlib import Path
 import srt
 
-path = "./splitted/"
-
-# SRT looks like this:
-
-# 00:00:00,000 --> 00:00:04,000
-
-# From ffmpeg docs:
-
-# 2.3 Time duration
-
-# There are two accepted syntaxes for expressing time duration.
-
-# [-][HH:]MM:SS[.m...]
-
-# HH expresses the number of hours, MM the number of minutes for a maximum of 2 digits, and SS the number of seconds for a maximum of 2 digits. The m at the end expresses decimal value for SS.
-
-# or
-
-# [-]S+[.m...][s|ms|us]
-
-# S expresses the number of seconds, with the optional decimal part m. The optional literal suffixes ‘s’, ‘ms’ or ‘us’ indicate to interpret the value as seconds, milliseconds or microseconds, respectively.
-
-# In both expressions, the optional ‘-’ indicates negative duration. 
+out_path = "./splitted/"
 
 def get_subs(data):
-    
-    # print(data)
+    """ """
     subs = []
     for sub in srt.parse(data):
         # use case is no subs 10 hours or longer
@@ -48,19 +25,19 @@ def main():
         exit(1)
     data = ''.join(sys.stdin.readlines())
     try:
-        os.mkdir(path)
+        os.mkdir(out_path)
     except FileExistsError:
         pass
     # record command line args
     original_file = sys.argv[1]
 
     # create a template of the ffmpeg call in advance
-    cmd_string = "ffmpeg -i {original_file} -acodec copy -ss {start} -to {end} {path}{id}.wav"
+    cmd_string = "ffmpeg -y -i {original_file} -acodec copy -ss {start} -to {end} {out_path}{id}.wav"
 
     for line in get_subs(data):
         id = Path(original_file).stem + line['start'].split('.')[0].replace(':','')
         print(id)
-        command = cmd_string.format(original_file=original_file, start=line['start'], end=line['end'], path=path, id=id)
+        command = cmd_string.format(original_file=original_file, start=line['start'], end=line['end'], out_path=out_path, id=id)
         print(command)
 
         # use subprocess to execute the command in the shell
